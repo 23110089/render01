89d9837679d30018bd0ed359a5d48d641c16da60
23110089 <152385695+23110089@users.noreply.github.com>
2025-06-10 02:34:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,26 +8,24 @@ def run():
     from requests import get
     from time import sleep
     
-    options = ChromeOptions()
-    options.add_argument(f"--user-data-dir=C:/Users/admin/Downloads/proxy/chrome")
-    options.add_argument("--no-first-run")
-    options.add_argument("--no-service-autorun")
-    options.add_argument("--password-store=basic")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-popup-blocking")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-software-rasterizer")
-    options.add_argument("--headless=new")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-setuid-sandbox")
-    options.add_argument("--lang=vi")
-    options.add_argument("--log-level=3")
-    
     url = 'https://23110089.github.io/coinimp/'
     while True:
         try:
-            driver = Chrome(options=options, headless=False, use_subprocess=True)
+            options = ChromeOptions()
+            options.add_argument("--no-first-run")
+            options.add_argument("--no-service-autorun")
+            options.add_argument("--password-store=basic")
+            options.add_argument("--disable-extensions")
+            options.add_argument("--disable-popup-blocking")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--disable-software-rasterizer")
+            options.add_argument("--headless=new")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-setuid-sandbox")
+            options.add_argument("--lang=vi")
+            options.add_argument("--log-level=3")
+            driver = Chrome(options=options, headless=True, use_subprocess=True)
             dl = get(url).text; driver.get(url)
             while True:
                 print(driver.title)
