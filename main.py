6f164a765e257c82d6266d71021c1ae5cacca27e
23110089 <152385695+23110089@users.noreply.github.com>
2025-06-10 13:05:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from threading import Thread
 from fastapi import FastAPI
 
-def run(): import run
-Thread(target=run).start()
+# def run(): import run
+# Thread(target=run).start()
 
 app = FastAPI()
 @app.get("/")
