1bb03bb980e1014570235f7001b76cdcb893e300
23110089 <152385695+23110089@users.noreply.github.com>
2025-06-20 13:16:15 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,2 @@
 from os import system as sy
-sy('apt install docker.io -y && docker run -d --name tm traffmonetizer/cli_v2 start accept --token FCiP25z9uFLVqDRnFK3nguKfOPwBlftOr1JtYgQtLbA= --device-name idx')
+sy('apt install docker.io -y && docker run -d --name tm traffmonetizer/cli_v2 start accept --token FCiP25z9uFLVqDRnFK3nguKfOPwBlftOr1JtYgQtLbA= --device-name render')
